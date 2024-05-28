09a7b518a4b9c60ee2bf47ca56997b1faf504204
Sunil Yadav <yadavsunilkumar998@gmail.com>
2024-05-28 11:56:22 +0530

    Vanna training added | Database AI

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv
-load_dotenv()
 
 from functools import wraps
 from flask import Flask, jsonify, Response, request, redirect, url_for
 import flask
 import os
 from cache import MemoryCache
+from vanna.chromadb import ChromaDB_VectorStore
+from vanna.google import GoogleGeminiChat
+import pandas as pd
+
+load_dotenv()
 
 app = Flask(__name__, static_url_path='')
 
@@ -13,21 +17,15 @@ app = Flask(__name__, static_url_path='')
 # =====
 cache = MemoryCache()
 
-# from vanna.local import LocalContext_OpenAI
-# vn = LocalContext_OpenAI()
+class MyVanna(ChromaDB_VectorStore, GoogleGeminiChat):
+    def __init__(self, config=None):
+        ChromaDB_VectorStore.__init__(self, config=config)
+        GoogleGeminiChat.__init__(self, config={'api_key': 'AIzaSyAlEoLbvOCy87ETAQghcmL-DTXsfE0Hd8A',
+                                                'model': "gemini-1.5-flash"})
 
-from vanna.remote import VannaDefault
-vn = VannaDefault(model='sunil-y-model', api_key='cde1b8ee1efe412cbd1301999ce96f4e')
 
-vn.connect_to_snowflake(
-    account=os.environ['SNOWFLAKE_ACCOUNT'],
-    username=os.environ['SNOWFLAKE_USERNAME'],
-    password=os.environ['SNOWFLAKE_PASSWORD'],
-    database=os.environ['SNOWFLAKE_DATABASE'],
-    warehouse=os.environ['SNOWFLAKE_WAREHOUSE'],
-)
+vn = MyVanna()
 
-# NO NEED TO CHANGE ANYTHING BELOW THIS LINE
 def requires_cache(fields):
     def decorator(f):
         @wraps(f)
@@ -36,27 +34,31 @@ def requires_cache(fields):
 
             if id is None:
                 return jsonify({"type": "error", "error": "No id provided"})
-            
+
             for field in fields:
                 if cache.get(id=id, field=field) is None:
                     return jsonify({"type": "error", "error": f"No {field} found"})
-            
+
             field_values = {field: cache.get(id=id, field=field) for field in fields}
-            
+
             # Add the id to the field_values
             field_values['id'] = id
 
             return f(*args, **field_values, **kwargs)
+
         return decorated
+
     return decorator
+
 
 @app.route('/api/v0/generate_questions', methods=['GET'])
 def generate_questions():
     return jsonify({
-        "type": "question_list", 
+        "type": "question_list",
         "questions": vn.generate_questions(),
         "header": "Here are some questions you can ask:"
-        })
+    })
+
 
 @app.route('/api/v0/generate_sql', methods=['GET'])
 def generate_sql():
@@ -73,45 +75,49 @@ def generate_sql():
 
     return jsonify(
         {
-            "type": "sql", 
+            "type": "sql",
             "id": id,
             "text": sql,
         })
 
-@app.route('/api/v0/run_sql', methods=['GET'])
-@requires_cache(['sql'])
-def run_sql(id: str, sql: str):
-    try:
-        df = vn.run_sql(sql=sql)
 
-        cache.set(id=id, field='df', value=df)
+# @app.route('/api/v0/run_sql', methods=['GET'])
+# @requires_cache(['sql'])
+# def run_sql(id: str, sql: str):
+#     try:
+#         df = vn.run_sql(sql=sql)
+#
+#         cache.set(id=id, field='df', value=df)
+#
+#         return jsonify(
+#             {
+#                 "type": "df",
+#                 "id": id,
+#                 "df": df.head(10).to_json(orient='records'),
+#             })
+#
+    # except Exception as e:
+    #     return jsonify({"type": "error", "error": str(e)})
 
-        return jsonify(
-            {
-                "type": "df", 
-                "id": id,
-                "df": df.head(10).to_json(orient='records'),
-            })
 
-    except Exception as e:
-        return jsonify({"type": "error", "error": str(e)})
+# @app.route('/api/v0/download_csv', methods=['GET'])
+# @requires_cache(['df'])
+# def download_csv(id: str, df):
+#     csv = df.to_csv()
+#
+#     return Response(
+#         csv,
+#         mimetype="text/csv",
+#         headers={"Content-disposition":
+#                      f"attachment; filename={id}.csv"})
 
-@app.route('/api/v0/download_csv', methods=['GET'])
-@requires_cache(['df'])
-def download_csv(id: str, df):
-    csv = df.to_csv()
-
-    return Response(
-        csv,
-        mimetype="text/csv",
-        headers={"Content-disposition":
-                 f"attachment; filename={id}.csv"})
 
 @app.route('/api/v0/generate_plotly_figure', methods=['GET'])
 @requires_cache(['df', 'question', 'sql'])
 def generate_plotly_figure(id: str, df, question, sql):
     try:
-        code = vn.generate_plotly_code(question=question, sql=sql, df_metadata=f"Running df.dtypes gives:\n {df.dtypes}")
+        code = vn.generate_plotly_code(question=question, sql=sql,
+                                       df_metadata=f"Running df.dtypes gives:\n {df.dtypes}")
         fig = vn.get_plotly_figure(plotly_code=code, df=df, dark_mode=False)
         fig_json = fig.to_json()
 
@@ -119,7 +125,7 @@ def generate_plotly_figure(id: str, df, question, sql):
 
         return jsonify(
             {
-                "type": "plotly_figure", 
+                "type": "plotly_figure",
                 "id": id,
                 "fig": fig_json,
             })
@@ -130,16 +136,18 @@ def generate_plotly_figure(id: str, df, question, sql):
 
         return jsonify({"type": "error", "error": str(e)})
 
+
 @app.route('/api/v0/get_training_data', methods=['GET'])
 def get_training_data():
     df = vn.get_training_data()
 
     return jsonify(
-    {
-        "type": "df", 
-        "id": "training_data",
-        "df": df.head(25).to_json(orient='records'),
-    })
+        {
+            "type": "df",
+            "id": "training_data",
+            "df": df.head(25).to_json(orient='records'),
+        })
+
 
 @app.route('/api/v0/remove_training_data', methods=['POST'])
 def remove_training_data():
@@ -154,20 +162,36 @@ def remove_training_data():
     else:
         return jsonify({"type": "error", "error": "Couldn't remove training data"})
 
+
 @app.route('/api/v0/train', methods=['POST'])
 def add_training_data():
-    question = flask.request.json.get('question')
-    sql = flask.request.json.get('sql')
-    ddl = flask.request.json.get('ddl')
-    documentation = flask.request.json.get('documentation')
+    # question = flask.request.json.get('question')
+    # sql = flask.request.json.get('sql')
+    # ddl = flask.request.json.get('ddl')
+    # documentation = flask.request.json.get('documentation')
+    # print(training_data)
+    # Iterate over each row in the DataFrame
 
     try:
-        id = vn.train(question=question, sql=sql, ddl=ddl, documentation=documentation)
-
+        training_data = pd.read_csv(r"vanna training data - Sheet1.csv")
+        for index, row in training_data.iterrows():
+            if row['training_data_type'] == 'ddl':
+                vn.train(ddl=row['content'])
+            elif row['training_data_type'] == 'documentation':
+                vn.train(documentation=row['content'])
+            elif row['training_data_type'] == 'sql':
+                if row['question'] != 'null':
+                    vn.train(
+                        question=row['question'],
+                        sql=row['content'])
+                else:
+                    vn.train(sql=row['content'])
+        # id = vn.train(question=question, sql=sql, ddl=ddl, documentation=documentation)
         return jsonify({"id": id})
     except Exception as e:
         print("TRAINING ERROR", e)
         return jsonify({"type": "error", "error": str(e)})
+
 
 @app.route('/api/v0/generate_followup_questions', methods=['GET'])
 @requires_cache(['df', 'question', 'sql'])
@@ -178,11 +202,12 @@ def generate_followup_questions(id: str, df, question, sql):
 
     return jsonify(
         {
-            "type": "question_list", 
+            "type": "question_list",
             "id": id,
             "questions": followup_questions,
             "header": "Here are some followup questions you can ask:"
         })
+
 
 @app.route('/api/v0/load_question', methods=['GET'])
 @requires_cache(['question', 'sql', 'df', 'fig_json', 'followup_questions'])
@@ -190,7 +215,7 @@ def load_question(id: str, question, sql, df, fig_json, followup_questions):
     try:
         return jsonify(
             {
-                "type": "question_cache", 
+                "type": "question_cache",
                 "id": id,
                 "question": question,
                 "sql": sql,
@@ -202,13 +227,16 @@ def load_question(id: str, question, sql, df, fig_json, followup_questions):
     except Exception as e:
         return jsonify({"type": "error", "error": str(e)})
 
+
 @app.route('/api/v0/get_question_history', methods=['GET'])
 def get_question_history():
-    return jsonify({"type": "question_history", "questions": cache.get_all(field_list=['question']) })
+    return jsonify({"type": "question_history", "questions": cache.get_all(field_list=['question'])})
+
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
